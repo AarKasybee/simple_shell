acd858ccc42ebe026005235a9606d71756341e0b
root <Beethoven Kasompe>
2023-05-12 18:45:15 -0700

    Tasks 0 - 5

---
+++ exit.c
@@ -0,0 +1,16 @@
+#include "headers.h"
+
+/**
+ * exit_func - This function exits the terminal or the shell
+ * @command: user input command
+ *
+ * Return: nothing
+ */
+void exit_func(char **command)
+{
+	if (strcmp(command[0], "exit") == 0) /* handle exit command */
+	{
+		exit(0);
+	}
+}
+
