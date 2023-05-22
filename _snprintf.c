43f13cba32ad185ff32cb64f1841b812905c2567
root <Beethoven Kasompe>
2023-05-21 17:28:24 -0700

    _snprintf creation

---
+++ _snprintf.c
@@ -0,0 +1,12 @@
+#include "headers.h"
+
+/**
+ * _snprintf - works like printf, but stores the values in a buffer array
+ * @input_str: input string
+ * @str_buf: buffer to store the string
+ *
+ * Return: void
+ */
+void _snprintf(char input_str, char *str_buf)
+{
+}
