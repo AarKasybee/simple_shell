06bef4bbc7f05d06b2a3ad838597ce3953d7034c
root <Beethoven Kasompe>
2023-05-24 15:20:18 -0700

    _snprintf added

--- fork_new_process.c
+++ fork_new_process.c
@@ -23,8 +23,8 @@ void fork_new_process(char **command, char **path)
 
 		for (i = 0; path[i] != NULL; i++)
 		{
-			/*_snprintf(path[i], command[0], executable_path);*/
-			snprintf(executable_path, MAX_PATH_LENGTH, "%s/%s", path[i], command[0]);
+			_snprintf(path[i], command[0], executable_path);
+			/*snprintf(executable_path, MAX_PATH_LENGTH, "%s/%s", path[i], command[0]);*/
 			/*_snprintf(path[i], command[0], executable_path);*/
 			if (access(executable_path, X_OK) == 0)
 			{
@@ -57,8 +57,8 @@ void fork_new_process(char **command, char **path)
 		execve(command[0], command, NULL);
 		for (i = 0; path[i] != NULL; i++)
 		{
-			/*_snprintf(path[i], command[0], executable_path);*/
-			snprintf(executable_path, MAX_PATH_LENGTH, "%s/%s", path[i], command[0]);
+			_snprintf(path[i], command[0], executable_path);
+			/*snprintf(executable_path, MAX_PATH_LENGTH, "%s/%s", path[i], command[0]);*/
 			/*_snprintf(path[i], command[0], executable_path);*/
 			execve(executable_path, command, NULL);
 		}
