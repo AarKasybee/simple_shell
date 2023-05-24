0b10d00cc185bf39cf4f4445a1be2decea2e3c3e
root <Beethoven Kasompe>
2023-05-24 15:57:02 -0700

    warming

--- fork_new_process.c
+++ fork_new_process.c
@@ -1,6 +1,6 @@
 #include "headers.h"
 
-void call_parent(char **command, pid_t pid);
+void call_parent(pid_t pid);
 
 /**
  * fork_new_process - forks a new process within the parent process
@@ -33,12 +33,15 @@ void fork_new_process(char **command, char **path)
 		}
 		if (path[i] == NULL)
 		{
-			_puts("Command ");
-			_puts("'");
-			_puts(command[0]);
-			_puts("'");
-			_puts(" not found");
+			_puts("./shell: No such file or directory");
 			_puts("\n");
+			/**
+			 * _puts("'");
+			 * _puts(command[0]);
+			 *_puts("'");
+			 *_puts(" not found");
+			 *_puts("\n");
+			 */
 			return;
 		}
 	}
@@ -67,7 +70,7 @@ void fork_new_process(char **command, char **path)
 	}
 	else /*parent process*/
 	{
-		call_parent(command, pid);
+		call_parent(pid);
 	}
 }
 
@@ -77,7 +80,7 @@ void fork_new_process(char **command, char **path)
  * @pid: process id
  * Return: void
  */
-void call_parent(char **command, pid_t pid)
+void call_parent(pid_t pid)
 {
 	int status;
 
@@ -91,13 +94,17 @@ void call_parent(char **command, pid_t pid)
 
 		if (exit_status != 0)
 		{
-			_puts("Command ");
-			_puts("'");
-			_puts(command[0]);
-			_puts("'");
-			_puts(" failed with exit status ");
-			_puts(_exit_status);
+			_puts("./shell: No such file or directory");
 			_puts("\n");
+			/**
+			 * _puts("Command ");
+			 *_puts("'");
+			 *_puts(command[0]);
+			 *_puts("'");
+			 *_puts(" failed with exit status ");
+			 *_puts(_exit_status);
+			 *_puts("\n");
+			 */
 		}
 	}
 	else if (WIFSIGNALED(status))
@@ -106,12 +113,7 @@ void call_parent(char **command, pid_t pid)
 		char _signal_number[10];
 
 		int_to_str(signal_number, _signal_number);
-		_puts("Command ");
-		_puts("'");
-		_puts(command[0]);
-		_puts("'");
-		_puts(" was terminated by signal ");
-		_puts(_signal_number);
+		_puts("./shell: No such file or directory");
 		_puts("\n");
 	}
 }
