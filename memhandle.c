9714c579e15c753285c0708c9554735f73fae073
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-05-26 01:31:02 -0700

    fixes

--- memhandle.c
+++ memhandle.c
@@ -126,21 +126,4 @@ void freeSarray(char **arr, int size)
 	}
 	free(arr);
 }
-/**
- * checkOnlySpaces - check if only spaces
- * @line: data holder
- * Return: 1 (not spaces) | 0 (only spaces)
- */
-int checkOnlySpaces(char *line)
-{
-	int i = 0;
-
-	while (line[i])
-	{
-		if (line[i] != ' ')
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
